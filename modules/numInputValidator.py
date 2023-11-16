fcccdba6bdc41f525ff0082fd103ded87999b44c
Ugochukwu Eke <96075072+Jay4you@users.noreply.github.com>
2023-11-16 23:10:55 +0100

    Num validator updated

--- modules/numInputValidator.py
+++ modules/numInputValidator.py
@@ -1,7 +1,20 @@
 from sys import exit
-
-
+from inputCollector import key
 def wishToBeMod():
     if __name__ == "numInputValidator":
         exit()
 wishToBeMod()
+
+'''
+This function validates numeric characters
+'''
+def numVal():
+    try:
+        plKey = key()
+        assert plKey.numeric
+        return plKey
+    except:
+        print("Please enter a valid number between 1 - 25!")
+        numVal()
+
+
